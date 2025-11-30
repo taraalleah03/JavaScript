90395b317b2e896e95592c23274746e0f978419c
tarahere <mtaraalleah@gmail.com>
2025-11-30 14:55:06 +0200

    update

--- Exercise_sets/Module_3/3-2/t2/2.js
+++ Exercise_sets/Module_3/3-2/t2/2.js
@@ -1,9 +1,17 @@
-const list = document.createElement("li");
-list.innerText =
-    `<li>First item</li>
-    <li>Second item</li>
-    <li>Third item</li>`;
+const list = document.getElementById("#target");
 
-document.body.appendChild(list);
+const list1 = document.createElement("li");
+list1.innerText = "First item"
 
-document.getElementById("target").classList.add("my-list");
+const list2 = document.createElement("li");
+list2.innerText = "Second item"
+
+const list3 = document.createElement("li");
+list3.innerText = "Third item"
+
+list2.classList.add("my-item")
+
+document.body.appendChild(list1);
+document.body.appendChild(list2);
+document.body.appendChild(list3);
+
