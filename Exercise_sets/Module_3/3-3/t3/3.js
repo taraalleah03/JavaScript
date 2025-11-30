0ae390da93cbddf278898861f7d69722080b9ff7
tarahere <mtaraalleah@gmail.com>
2025-11-30 17:45:28 +0200

    update

--- Exercise_sets/Module_3/3-3/t3/3.js
+++ Exercise_sets/Module_3/3-3/t3/3.js
@@ -1,2 +1,10 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+let name = "";
+
+for (let i = 0; i < 3 ; i++){
+    name += `<li>${names[i]}<br></li>`;
+}
+
+document.getElementById("target").innerHTML = name;
