8434e332fd6aedf1a3df85061a55a33324babd7b
tarahere <mtaraalleah@gmail.com>
2025-11-30 14:36:44 +0200

    update

---
+++ Exercise_sets/Module_3/3-1/t1/1.js
@@ -0,0 +1,6 @@
+let html = document.getElementById("target").innerHTML =
+    `<li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>`;
+
+document.getElementById("target").classList.add("my-list");
